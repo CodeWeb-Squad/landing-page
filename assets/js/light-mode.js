14eb3268eab67e5b9c70a0fcda2d864e719ff884
FrancioXavier <103940118+FrancioXavier@users.noreply.github.com>
2022-12-01 16:10:04 -0300

    light-mode, footer and Raissa section adjust

---
+++ assets/js/light-mode.js
@@ -0,0 +1,19 @@
+
+
+var a = document.getElementById('button-light');
+
+a.addEventListener('click', lightMode)
+
+function lightMode() {
+    var body = document.body;
+    var home = document.getElementById('home')
+
+    home.classList.toggle('light-bg')
+    body.classList.toggle('light-mode')
+    for (var i = 0, len = 5; i < len; i++) {
+        var section = document.getElementById(`light-${i + 1}`)
+        section.classList.toggle('dark-sec')
+        section.querySelector('p').classList.add('dark-sec')
+        section.querySelector('h2').classList.add('dark-sec')
+    }
+}
